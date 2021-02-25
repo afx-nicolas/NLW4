35a6c60c1bddb184f41c7ee624cefe64977f775f
afx-nicolas <afxnicolas@gmail.com>
2021-02-25 20:17:14 -0300

    Dia 04

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,18 @@
-import styles from '../styles/components/Profile.module.css'
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/afx-nicolas.png" alt="Nicolas"/>
+      <img src="https://github.com/afx-nicolas.png" alt="Nicolas" />
       <div>
         <strong>Nicolas A. Feitoza</strong>
         <p>
-          <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
         </p>
       </div>
     </div>
